6cbdf6eea8308f6810e33420f33d4fdcb58cf531
MassimilianoMaccione <massimiliano.maccione@studenti.isissgobetti.it>
2024-10-08 13:37:09 +0200

    miequery

---
+++ mie.sql
@@ -0,0 +1,18 @@
+-- -- Every album
+-- SELECT * FROM albums;
+
+-- Every album title (and only the title)
+-- SELECT title FROM albums;
+
+-- Every invoice
+-- SELECT * FROM invoices;
+
+-- Every invoice ordered by total invoice amount ("total")
+-- SELECT * FROM invoices ORDER BY total;
+
+-- Every invoice with a total greater than 10
+-- SELECT * FROM invoices ORDER BY total > 10;
+
+-- The 10 most expensive invoices
+-- SELECT * FROM invoices ORDER BY total DESC LIMIT 10;
+
